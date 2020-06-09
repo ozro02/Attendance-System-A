5eaa35b86e5227e1456554fb48e8061768ab3e8c
ozro02 <kjash02@gmail.com>
2020-06-09 16:03:30 +0000

    次回は上長を作成する。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,22 @@ User.create!(name: "Sample User",
              password: "password",
              password_confirmation: "password",
              admin: true)
+             
+User.create!(name: "上長A",
+             email: "sample-1@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: false)      
+             
+User.create!(name: "上長B",
+             email: "sample-2@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: false)                   
 
 60.times do |n|
   name  = Faker::Name.name
-  email = "sample-#{n+1}@email.com"
+  email = "sample-#{n+3}@email.com"
   password = "password"
   User.create!(name: name,
                email: email,
