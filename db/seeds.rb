7d5624fcdbfbc3f23fb6261276928358823b5f5a
ozro02 <kjash02@gmail.com>
2020-06-10 23:04:43 +0000

    ユーザー一覧編集中。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-User.create!(name: "Sample User",
+User.create!(name: "管理者",
              email: "sample@email.com",
              password: "password",
              password_confirmation: "password",
@@ -9,21 +9,30 @@ User.create!(name: "Sample User",
 User.create!(name: "上長A",
              email: "sample-1@email.com",
              password: "password",
-             password_confirmation: "password",
-             admin: false)      
+             password_confirmation: "password")
              
 User.create!(name: "上長B",
              email: "sample-2@email.com",
              password: "password",
-             password_confirmation: "password",
-             admin: false)                   
+             password_confirmation: "password")
 
-60.times do |n|
-  name  = Faker::Name.name
-  email = "sample-#{n+3}@email.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
+User.create!(name: "社員1",
+             email: "sample-3@email.com",
+             password: "password",
+             password_confirmation: "password")
+             
+User.create!(name: "社員2",
+             email: "sample-4@email.com",
+             password: "password",
+             password_confirmation: "password")              
+             
+             
+#60.times do |n|
+#  name  = Faker::Name.name
+#  email = "sample-#{n+1}@email.com"
+#  password = "password"
+#  User.create!(name: name,
+#               email: email,
+#               password: password,
+#               password_confirmation: password)
+#end
