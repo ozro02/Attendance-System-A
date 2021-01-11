4d691be584f95382b11480e08e8bda910f7ae459
ozro02 <kjash02@gmail.com>
2021-01-11 07:40:15 +0000

    モーダル表示完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :affiliation, length: { in: 2..30 }, allow_blank: true
-  validates :basic_work_time, presence: true
-  validates :designated_work_start_time, presence: true
-  validates :designated_work_end_time, presence: true
+  validates :basic_time, presence: true
+  # validates :designated_work_start_time, presence: true
+  # validates :designated_work_end_time, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
